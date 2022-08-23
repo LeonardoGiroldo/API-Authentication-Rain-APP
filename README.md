9d582bc327ae94f8046ff651136d8f1023a2e8e8
Leonardo Giroldo <leonardo.giroldo@tui.com>
2022-08-23 23:17:30 +0200

    uploaded final version of the code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # API-Authentication-Rain-APP
+
+This project is to practice on how to work with APIs, create enviroment variables and deploy the project in python anywhere free tier
---
+++ main.py
@@ -0,0 +1,45 @@
+import requests
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = "325bea58cc447e5be8f6281cbe92173e"
+end_point = "https://api.openweathermap.org/data/2.5/forecast?"
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+
+
+parameters = {
+    "lat" : 39.585676,
+    "lon" : 2.679648,
+    "appid" : api_key,
+    "units" : "metric"
+}
+
+response = requests.get(end_point, params= parameters)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data['list'][:5]
+
+will_rain = False
+
+for each_item in weather_slice:
+    condition_code = each_item['weather'][0]['id']
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+    client = Client(account_sid, auth_token, http_client=proxy_client)
+    message = client.messages.create(
+                              body='It is going to rain today. Remember to bring an umbrella!',
+                              from_='whatsapp:+14155238886',
+                              to='whatsapp:+34610608797'
+                          )
+
+    print(message.status)
